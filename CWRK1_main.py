1962b446f9a61b70f71bdf936a7137b98c9c5fca
John-Louie <john_louie@icloud.com>
2023-03-15 14:31:58 +0000

    Updated Menu with Template

--- CWRK1_main.py
+++ CWRK1_main.py
@@ -1,32 +1,41 @@
-#import modules/packages
+import pandas as pd
+import matplotlib as plt
+import csv
 
 #display data from csv
+df = pd.read_csv("")
+
+#EVERYONE'S VISUALISATION
 
 #main menu
+#for some reason python 3.9 doesnt have a switch case option, needs python 3.10
 while (True):
     print("-----Menu-----\nSelect one of the choices to see a visualisation:\n")
     choice = input("1 - Visual\n2 - Visual2\n3 - Visual3\n4 - Visual4\n5 - Visual5\n6 - Visual6\nQ - Quit\n\nSelect: ")
     choice = choice.upper()
 
-    match choice:
-        case "1":
-            print("calls function of visual 1")
-        case "2":
-            print("calls function of visual 2")
-        case "3":
-            print("calls function of visual 3")
-        case "4":
-            print("calls function of visual 4")
-        case "5":
-            print("calls function of visual 5")
-        case "6":
-            print("calls function of visual 6")
-        case "Q":
-            print("\nThank you for using the data visualisation program, Have a nice day!\n")
-            break
-        case _:
-            print(f"\nInvalid input:{choice}, select one of the choices provided\n")
-            continue
+    if choice == "1":
+        print("calls function of visual 1")
         
-
-#everyones individual visualisations 
+    elif choice ==  "2":
+        print("calls function of visual 2")
+        
+    elif choice ==  "3":
+        print("calls function of visual 3")
+        
+    elif choice ==  "4":
+        print("calls function of visual 4")
+        
+    elif choice ==  "5":
+        print("calls function of visual 5")
+        
+    elif choice ==  "6":
+        print("calls function of visual 6")
+        
+    elif choice ==  "Q":
+        print("\nThank you for using the data visualisation program, Have a nice day!\n")
+        exit()
+        
+    else:
+        print(f"\nInvalid input:{choice}, select one of the choices provided\n")
+        continue
