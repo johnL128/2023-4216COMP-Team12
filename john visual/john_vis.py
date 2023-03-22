1ec498f951989b3459d485cc0c14b03b38b79207
johnL128 <john_louie@icloud.com>
2023-03-22 18:55:01 +0000

    convert code to name for graph title

--- john visual/john_vis.py
+++ john visual/john_vis.py
@@ -11,24 +11,26 @@ print(f"\n--- Enter \'Q\' to quit the program ---")
 
 while (True):
     sel = input("\nEnter the Country Code: ")
-    select_count = sel.title()
+    sel_code = sel.upper()
     
-    if w_df["Country Name"].eq(select_count).any() and ut_df["Country Name"].eq(select_count).any():
+    if w_df["Country Code"].eq(sel_code).any() and ut_df["Country Code"].eq(sel_code).any():
+        convert = w_df.loc[w_df["Country Code"] == sel_code]
+        cou_name = convert["Country Name"].values[0]
         break
     
-    elif select_count == "Q":
+    elif sel_code == "Q":
         exit()
     
     else:
-        print(f"\nCountry Name doesn't exist within the database/\"{select_count}\" isn't a real country")
+        print(f"\nCountry Code doesn't exist within the database/\"{sel_code}\" isn't a real country")
 
 while (True):
-    s_year = input("\nEnter a year: ")
+    s_year = input("\nEnter a beginning year: ")
 
     if s_year >="1960" and s_year <= "2020":
         break
     
-    elif select_count == "Q":
+    elif s_year == "Q" or s_year == "q":
         exit()
     
     else:
@@ -37,23 +39,23 @@ while (True):
 while (True):
     s_year2 = input("\nEnter another year: ")
     
-    if s_year >= "1960" and s_year <= "2020":
+    if s_year >= "1960" and s_year <= "2020" and s_year2 != s_year and s_year2 > s_year:
         break
     
-    elif select_count == "Q":
+    elif s_year == "Q" or s_year == "q":
         exit()
-    
+        
     else:
         print(f"\n{s_year} doesn't exist in this database!")
 
 #manipulate both csv files, get data
-ut = ut_df.loc[ut_df["Country Name"] == select_count, s_year:s_year2]
-rur = w_df.loc[w_df["Country Name"] == select_count, s_year:s_year2] - ut
+ut = ut_df.loc[ut_df["Country Code"] == sel_code, s_year:s_year2]
+rur = w_df.loc[w_df["Country Code"] == sel_code, s_year:s_year2] - ut
 
-print(f"\n--- Urban Data for {select_count} between {s_year}-{s_year2} ---\n")
+print(f"\n--- Urban Data for {sel_code} between {s_year}-{s_year2} ---\n")
 print(ut)
 print(" ")
-print(f"\n--- Rural Data for {select_count} between {s_year}-{s_year2} ---\n")
+print(f"\n--- Rural Data for {sel_code} between {s_year}-{s_year2} ---\n")
 print(rur)
 
 #plot both lines on the graph
@@ -61,7 +63,7 @@ fig, ax = plt.subplots()
 line1 = ax.plot(ut.columns.T, ut.T, color='r')
 line2 = ax.plot(rur.columns.T, rur.T, color='b')
 
-fig.suptitle(f"Urban and Rural Data for {select_count} between {s_year}-{s_year2}")
+fig.suptitle(f"Urban and Rural Data for {cou_name} between {s_year}-{s_year2}")
 fig.supxlabel("Years")
 fig.supylabel("Population")
 ax.get_yaxis().get_major_formatter().set_scientific(False)
