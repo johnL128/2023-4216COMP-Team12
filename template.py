1f87217eabe31477d0336ffafb817e356ad7bb92
johnL128 <118545645+johnL128@users.noreply.github.com>
2023-03-23 12:11:18 +0000

    rearranged template

--- template.py
+++ template.py
@@ -4,11 +4,11 @@
 #Importing CSV
 ###
 
+#if you want a menu insert it here
+###
+
 #Selection of data using pandas
 ###
 
 #Code for plotting matplotlib
-###
-
-#if you want a menu insert it below here
 ###
