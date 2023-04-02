e6a09442311b7ac4faba01ea146593abfbf961a2
johnL128 <john_louie@icloud.com>
2023-04-02 12:10:11 +0100

    implemented new version

--- CWRK1_main.py
+++ CWRK1_main.py
@@ -118,8 +118,8 @@ def comparingCountries():
 
 #John's Visualisation
 def compareUrbanRural():
-    w_df = pd.read_csv("csv/world.csv")
     ut_df = pd.read_csv("csv/urban_total.csv")
+    w_df = pd.read_csv("csv/world.csv")
 
     print(f"\n--- Compare a selected country\'s urban/rural population over a selected time period ---")
     print(f"\n--- Enter \'Q\' to quit the program ---")
@@ -158,54 +158,61 @@ def compareUrbanRural():
         else:
             print(f"\n{s_year} doesn't exist in this database!")
 
-
-    #GATHER DATA
+    #GATHER DATA, SET PARAMETERS TO AVOID NaN VALUES
+    wrl = w_df.loc[w_df["Country Code"] == sel_code, s_year:s_year2]
     ut = ut_df.loc[ut_df["Country Code"] == sel_code, s_year:s_year2]
-    rur = w_df.loc[w_df["Country Code"] == sel_code, s_year:s_year2] - ut
+    wrl.reset_index(drop=True, inplace=True)
+    ut.reset_index(drop=True, inplace=True)
+    rur = wrl - ut
 
     while (True):
         try:
             print("\nSelect a visualisation option:")
-            graph_choice = int(input("\n1 - Compare Urban and Rural\n2 - Urban and Rural Population ONLY\nChoice: "))
+            graph_choice = int(input("\n1 - Compare Urban and Rural\n2 - Urban and Rural Population ONLY\n\nChoose: "))
         
         except ValueError as ve:
             print(f"\nInvalid choice, it must be a NUMBER!")
             continue
         
-        fig, ax = plt.subplots(graph_choice)
-        fig.suptitle(f"Urban and Rural Data for {cou_name} between {s_year}-{s_year2}", fontsize=20)
-        fig.supxlabel("Years", fontsize=20)
-        fig.supylabel("Population", fontsize=20)
-        urb_leg = mpatches.Patch(color='red', label='Urban Population')
-        rur_leg = mpatches.Patch(color='blue', label='Rural Population')
         
         if graph_choice == 1:
-            ax.plot(ut.columns.T, ut.values.T, color='r')
-            ax.plot(rur.columns.T, rur.values.T, color='b')
+            fig, ax = plt.subplots(graph_choice)
+            ax.plot(ut.columns.T, ut.values.T, 'r')
+            ax.plot(rur.columns.T, rur.values.T, 'b')
             ax.get_yaxis().get_major_formatter().set_scientific(False)
             ax.tick_params('x',labelrotation=90)
+            fig.supxlabel("Years", fontsize=20)
+            fig.supylabel("Population", fontsize=20)
+            fig.suptitle(f"Urban and Rural Comparison for {cou_name} between {s_year}-{s_year2}", fontsize=20)
+            
+            #Set legend
+            urb_leg = mpatches.Patch(color='red', label='Urban Population')
+            rur_leg = mpatches.Patch(color='blue', label='Rural Population')
             ax.legend(handles=[urb_leg, rur_leg])
-            ax.grid()
             break
         
         elif graph_choice == 2:
-            ax[0].plot(ut.columns.T, ut.values.T, color='r')
-            ax[1].plot(rur.columns.T, rur.values.T, color='b')
+            fig, ax = plt.subplots(graph_choice)
+            ax[0].plot(ut.columns.T, ut.values.T, 'r')
+            ax[0].set_title(f"Urban Data for {cou_name} between {s_year}-{s_year2}", fontsize=20)
+            ax[1].plot(rur.columns.T, rur.values.T, 'b')
+            ax[1].set_title(f"Rural Data for {cou_name} between {s_year}-{s_year2}", fontsize=20)
             
             #format both graphs
             for i in range(graph_choice):
                 ax[i].get_yaxis().get_major_formatter().set_scientific(False)
                 ax[i].tick_params('x',labelrotation=90)
+                ax[i].set_xlabel("Years", fontsize=18)
+                ax[i].set_ylabel("Population", fontsize=18)
                 ax[i].grid()
-            
-            #seperate legends for each graph (different values)
-            ax[0].plot(ut.columns.T, ut.values.T, color='r')
-            ax[0].legend(handles=[urb_leg])
-            
-            ax[1].plot(rur.columns.T, rur.values.T, color='b')
-            ax[1].legend(handles=[rur_leg])
+                
+            plt.tight_layout()
             break
+        
+        else: 
+            print(f"\n\'{graph_choice}\' is not an option, please select from the above")
 
+    #Show Dataframe and visualisation
     print(f"\n--- Urban Data for {cou_name} between {s_year}-{s_year2} ---\n")
     print(ut)
     print("")
