94dec65e57c84274aec79c8c0e0f5ac3daf1df90
AdinaHera <csmarete@ljmu.ac.uk>
2023-04-04 17:44:26 +0100

    another start

--- Adina/Adina.py
+++ Adina/Adina.py
@@ -1,5 +1,6 @@
 import pandas as pd 
+import matplotlib.pyplot as plt
 r_vfp = pd.read_csv ("csv/region_world.csv") 
 user_input = input ("What year?")   
-year = r_vfp["user_imput"]
-print(year)
+year2 = r_vfp.nsmallest (10, user_input)
+print(year2[["Region Name", "Region Code", user_input]])
