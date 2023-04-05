ffa29c23a7b8103f7b87f941356bd4e8bf4df70b
AdinaHera <csmarete@ljmu.ac.uk>
2023-04-05 16:16:17 +0100

    final version

--- Adina/Adina.py
+++ Adina/Adina.py
@@ -1,6 +1,17 @@
-import pandas as pd 
+import pandas as pd
 import matplotlib.pyplot as plt
 r_vfp = pd.read_csv ("csv/region_world.csv") 
-user_input = input ("What year?")   
+while (True):
+     user_input = input ("What year?")   
+     if user_input >= "1960" and user_input <= "2020": 
+          break
+     else: 
+          print ("The year entered is invalid. Please enter a year between 1960 and 2020")
 year2 = r_vfp.nsmallest (10, user_input)
-print(year2[["Region Name", "Region Code", user_input]])
+print(year2[["Region Name", "Region Code", user_input]]) 
+year2.plot(x = "Region Code", y= user_input, kind = "bar", legend = None)
+plt.xlabel ("Region Code", fontsize = 12)
+plt.xticks ( fontsize = 8)
+plt.ylabel ("Region Population", fontsize = 12)
+plt.title (f"10 Smallest Regions of {user_input}", fontsize = 14)
+plt.show()
