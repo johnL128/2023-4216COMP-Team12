9c199b9fa9ffcd97a3b6ff8a85248bf996bfd18c
oluwatoyin chinell olotu <stgoolot@ljmu.ac.uk>
2023-04-17 10:12:35 +0100

    completed and updated code

--- Chinell/Chinell.py
+++ Chinell/Chinell.py
@@ -27,15 +27,27 @@ while True:
                 # Display the first 5 rows of the resulting dataframe
                 print(cf_year.head(5))
 
-                # Plot a coloured histogram chart of the urban population for the top 5 smallest countries
-                plt.figure(figsize=(8, 6))
-                plt.title(f"Top 5 Smallest Urban Populations in {n}", fontsize=16)
-                plt.bar(cf_year[:5]['Country Name'], cf_year[:5][n], color=['red', 'orange', 'yellow', 'green', 'blue'])
-                plt.xlabel("Country", fontsize=14)
-                plt.ylabel("Urban Population", fontsize=14)
-                
-                plt.show()
+                # Ask the user which type of chart to plot
+                chart_type = input("Enter 'bar' or 'pie' to plot a bar chart or a pie chart respectively: ")
 
+                if chart_type == "bar":
+                    # Plot a coloured bar chart of the urban population for the top 5 smallest countries
+                    plt.figure(figsize=(8, 6))
+                    plt.title(f"Top 5 Smallest Urban Populations in {n}", fontsize=16)
+                    plt.bar(cf_year[:5]['Country Name'], cf_year[:5][n], color=['red', 'orange', 'yellow', 'green', 'blue'])
+                    plt.xlabel("Country", fontsize=14)
+                    plt.ylabel("Urban Population", fontsize=14)
+
+                elif chart_type == "pie":
+                    # Plot a coloured pie chart of the urban population for the top 5 smallest countries
+                    plt.figure(figsize=(8, 6))
+                    plt.title(f"Top 5 Smallest Urban Populations in {n}", fontsize=16)
+                    plt.pie(cf_year[:5][n], labels=cf_year[:5]['Country Name'], colors=['red', 'orange', 'yellow', 'green', 'blue'], autopct='%1.1f%%', startangle=90)
+
+                else:
+                    print("Incorrect input. Please enter 'bar' or 'pie'.")
+
+                plt.show()
 
     elif choice == "2":
         print("Exiting the program...")
